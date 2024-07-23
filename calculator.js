67051f753928b1454260fc8904bac127716a8c04
ISHU456 <147750433+ISHU456@users.noreply.github.com>
2024-07-23 15:12:56 +0530

    calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,21 @@
 const display = document.getElementById("display");
 
-functio
+function appendToDisplay(input){
+    display.value += input;
+}
+function clearValue(){
+    display.value ="";
+}
+function delValue(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "ERROR";
+    }
+    
+}
